b29d6d2f2c3a80725a5593dffa4a7deab476e16e
ToniMaciel <jaam@cin.ufpe.br>
2022-12-21 19:13:12 -0300

    feat: resolução questão 1

---
+++ Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Solucao
+{
+    public static class Utils
+    {
+        public static int LerValorInteiro()
+        {
+            string? valorLido = Console.ReadLine();
+            int valor;
+
+            while (!int.TryParse(valorLido, out valor) || valor <= 0)
+            {
+                Console.Write($"O valor \"{valorLido}\" não é um número inteiro positivo. Por favor, infome o valor novamente: ");
+                valorLido = Console.ReadLine();
+            }
+
+            return valor;
+        }
+    }
+}
