525a6b23f9f8e77a4327e23941045f7d70ed9bf9
ToniMaciel <jaam@cin.ufpe.br>
2022-12-21 22:30:05 -0300

    feat: solução questão 3

--- Solução/Program.cs
+++ Solução/Program.cs
@@ -1,24 +1,54 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
-using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Solucao;
 
 class Primeira
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        string path_desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        var distancias_cidades = File.ReadAllLines(path_desktop + Path.DirectorySeparatorChar + "matriz.txt")
-            .Select(s => s.Split(',').Select(n => Int32.Parse(n)).ToArray())
-            .ToArray();
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = false,
+        };
 
-        var caminho = File.ReadLines(path_desktop + Path.DirectorySeparatorChar + "caminho.txt").First()
-            .Split(", ")
-            .Select(n => Int32.Parse(n)).ToArray();
+        string path_desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        using var reader = new StreamReader(path_desktop + Path.DirectorySeparatorChar + "matriz.txt");
+        using var csv = new CsvParser(reader, config);
+
+        if (!csv.Read())
+            return;
+
+        var numColunas = csv.Record.Length;
+        var distancias_cidades = new int[numColunas, numColunas];
+
+        for (int i = 0; i < numColunas; i++)
+        {
+            var linha = csv.Record;
+
+            for (int j = 0; j < numColunas; j++)
+                distancias_cidades[i, j] = int.Parse(linha[j]);
+
+            csv.Read();
+        }
+
+        using var reader_caminho = new StreamReader(path_desktop + Path.DirectorySeparatorChar + "caminho.txt");
+        using var csv_caminho = new CsvParser(reader_caminho, config);
+
+        if (!csv_caminho.Read())
+            return;
+
+        int cidades_percorridas = csv_caminho.Record.Length;
+        int[] caminho = new int[cidades_percorridas];
+
+        for (int i = 0; i < cidades_percorridas; i++)
+            caminho[i] = int.Parse(csv_caminho[i]);
 
         int soma_distancia = 0;
-
         for (int i = 1; i < caminho.Length; i++)
-            soma_distancia += distancias_cidades[caminho[i - 1] - 1][caminho[i] - 1];
+            soma_distancia += distancias_cidades[caminho[i - 1] - 1, caminho[i] - 1];
 
         Console.WriteLine($"A distância total percorrida foi de {soma_distancia} km.");
     }
